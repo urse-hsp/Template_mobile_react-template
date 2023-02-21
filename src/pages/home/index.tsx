a864bc64d5ab8f243368c4d6dca52250a0171454
p <email>
2023-02-21 16:25:31 +0800

    style: init

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import models from '@/models';
 import utils from '@/models/utils';
 import Web3Provider from '@/models/Web3Provider';
+import './index.scss';
 
 interface IndexType {
   isVisible?: boolean;
@@ -13,16 +14,16 @@ const Index: React.FC<IndexType> = (props) => {
   const res = utils.useContainer();
 
   return (
-    <>
-      {res.theme} home2
+    <div className="home">
+      {res.theme} home2家里
       <button
         onClick={() => {
           res.setTheme('123');
         }}
       >
-        dianji
+        dianji就这啊
       </button>
-    </>
+    </div>
   );
 };
 export default Index;
